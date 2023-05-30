4a89c2c7ddd62ed133073420f6a91eb73383fd13
jruss3 <jruss3@chewy.com>
2023-05-30 09:59:16 -0500

    Add test explanation and presentation PDF

--- pet-store-api/src/test/kotlin/com/chewy/osn/response/CutenessResponseTest.kt
+++ pet-store-api/src/test/kotlin/com/chewy/osn/response/CutenessResponseTest.kt
@@ -27,6 +27,9 @@ class CutenessResponseTest: FreeSpec({
         //assertion
         deserialized shouldBe expected
 
+        //Running this test forward and back is super useful when working with someone else's contract
+        //Hit their API, grab a sample response, then test like this to ensure all fields are ingested as expected
+        //Going back to JSON ensures we didn't mutate/miss any fields in the payload
         //execution
         val backToJson = mapper.writeValueAsString(expected)
 
